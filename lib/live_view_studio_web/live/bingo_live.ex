99fc877ddda2c314cf35935e190e9eed377a04a5
Josue Velez <josuevelezdr@gmail.com>
2023-09-07 06:36:33 +0200

    feat: handle info for bingo

--- lib/live_view_studio_web/live/bingo_live.ex
+++ lib/live_view_studio_web/live/bingo_live.ex
@@ -2,6 +2,10 @@ defmodule LiveViewStudioWeb.BingoLive do
   use LiveViewStudioWeb, :live_view
 
   def mount(_params, _session, socket) do
+    if connected?(socket) do
+      :timer.send_interval(3000, self(), :tick)
+    end
+
     socket =
       assign(socket,
         number: nil,
@@ -45,5 +49,9 @@ defmodule LiveViewStudioWeb.BingoLive do
       Enum.map(numbers, &"#{letter} #{&1}")
     end)
     |> Enum.shuffle()
+  end
+
+  def handle_info(:tick, socket) do
+    {:noreply, pick(socket)}
   end
 end
